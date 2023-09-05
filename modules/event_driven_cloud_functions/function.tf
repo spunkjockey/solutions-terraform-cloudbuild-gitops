6e50668ce6ae972b3bc448d3de0c5a593d900e32
Oscar Vasquez <spunkjockey18@gmail.com>
2023-09-05 15:51:45 -0600

    Adding event_driven_cloud_functions module

--- modules/event_driven_cloud_functions/function.tf
+++ modules/event_driven_cloud_functions/function.tf
@@ -1,7 +1,7 @@
 # Generates an archiva of source code compressed as a .zip file.
 data "archive_file" "source" {
   type        = "zip"
-  source_dir  = "../../src/event_drive_cloud_functions"
+  source_dir  = "../../src/event_driven_cloud_functions"
   output_path = "${path.module}/function.zip"
 }
 
@@ -19,14 +19,14 @@ resource "google_storage_bucket_object" "zip" {
 
 # Create the Cloud function triggered by a `Finalize` event on the bucket
 resource "google_cloudfunctions_function" "cloud_function" {
-  name    = "cloud-function-triggered-using-terraform"
-  description = "Cloud Function will get triggered once file is uploaded in input-${var.env}-bucket"
-  runtime = "python39"
-  project = var.project
-  region = var.region
+  name                  = "cloud-function-triggered-using-terraform"
+  description           = "Cloud Function will get triggered once file is uploaded in input-${var.env}-bucket"
+  runtime               = "python39"
+  project               = var.project
+  region                = var.region
   source_archive_bucket = google_storage_bucket.cloud_function_bucket.name
   source_archive_object = google_storage_bucket_object.zip.name
-  entry_point = "fileUpload"
+  entry_point           = "fileUpload"
   event_trigger {
     event_type = "google.storage.object.finalize"
     resource   = "input-${var.env}-bucket"
